d7056af0366c491b1a6e33454ecb451f8924ae26
imandin408 <imandin408@gmail.com>
2018-06-05 11:42:47 -0400

    6/5/18 school pc

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,50 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureAtlas textureAtlas;
+	TextureRegion textureRegion;
+	Sprite sprite;
+	Animation<TextureRegion> stillAnimation;
+	Animation movingAnimation;
+	int frame = 1;
+	float timeForStill = 0.0f;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		textureAtlas = new TextureAtlas(Gdx.files.internal("megaSprites/megaAtlas.atlas"));
+		textureRegion = textureAtlas.findRegion("tile1");
+		sprite = new Sprite(textureRegion);
+		sprite.setPosition(Gdx.graphics.getWidth()/2-sprite.getWidth(),Gdx.graphics.getHeight()/2-sprite.getHeight());
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+
+		timeForStill += Gdx.graphics.getDeltaTime();
+		batch.draw(stillAnimation.getKeyFrame(timeForStill,true),0,0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
